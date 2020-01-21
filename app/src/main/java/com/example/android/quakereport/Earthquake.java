195e35637b228c40839da381f186e75fd179fece
Tsitohaina Rakotondramasy <tsitohaina.toetra.rak@gmail.com>
2020-01-21 21:52:40 +0300

    Created Earthquake object

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,54 @@
+package com.example.android.quakereport;
+
+/**
+ * {@link Earthquake} represents a single earthquake that happened somewhere in the world
+ * Each object has 3 attributes: city, magnitude, date
+ */
+
+public class Earthquake {
+
+    // name of the location where the earthquake happened
+    private String mCity;
+
+    //magnitude of the earthquake
+    private float mMagnitude;
+
+    //date at which the earthquake happened
+    private long mDate;
+
+    /**
+     * Constructor for the earthquake object
+     *
+     * @param city is the city where the earthquake happened
+     * @param mag  is the magnitude of the earthquake
+     * @param date is the date of the earthquake event
+     */
+    public Earthquake(String city, float mag, long date) {
+        mCity = city;
+        mMagnitude = mag;
+        mDate = date;
+    }
+
+    // Methods
+
+    /**
+     * Getter for the city
+     */
+    public String getCity() {
+        return mCity;
+    }
+
+    /**
+     * Getter for the magnitude
+     */
+    public float getMagnitude(){
+        return mMagnitude;
+    }
+
+    /**
+     * Getter for the date
+     */
+    public long getDate(){
+        return mDate;
+    }
+}
